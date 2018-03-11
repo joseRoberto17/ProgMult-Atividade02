4cf8df59e427fd6d40d5fd5b2fca6f904bf0ef2c
Jose <jose.roberto.xavier.s@gmail.com>
2018-03-11 12:23:21 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/aula02final/DialogActivity.java
@@ -0,0 +1,29 @@
+
+/**
+ * José Roberto Xavier da Silva
+ * 816113694
+ */
+
+package br.usjt.aula02final;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class DialogActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_dialog);
+    }
+
+    /**
+     * Callback method defined by the View
+     * @param v
+     */
+    public void finishDialog(View v) {
+        DialogActivity.this.finish();
+    }
+}
